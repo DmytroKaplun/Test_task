d6bd456216c3a4d28ca64ecf254258bc8f9af368
DmytroKaplun <newtonknight1990@gmail.com>
2024-10-10 17:59:04 +0300

    add Data

--- src/main/java/org/example/DocumentManager.java
+++ src/main/java/org/example/DocumentManager.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-
+@Data
 public class DocumentManager {
     private final Map<String, Document> documents = new HashMap<>();
 
